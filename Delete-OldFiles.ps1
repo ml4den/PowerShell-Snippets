33e7d2339323e7b8df854549ca9d04c27c6f52fa
ml4den <ml4den@users.noreply.github.com>
2020-06-22 09:39:24 +0100

    Update Delete-OldFiles.ps1

--- Delete-OldFiles.ps1
+++ Delete-OldFiles.ps1
@@ -1,6 +1,6 @@
 $Dir = "C:\Temp"
-$Daysback = "-30"
+$DaysBack = "-30"
  
 $CurrentDate = Get-Date
-$DateToDelete = $CurrentDate.AddDays($Daysback)
+$DateToDelete = $CurrentDate.AddDays($DaysBack)
 Get-ChildItem $Dir\*.txt | Where-Object { $_.LastWriteTime -lt $DateToDelete } | Remove-Item

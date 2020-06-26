a64084a5140e46a343e37729d90e420eff726c12
ml4den <ml4den@users.noreply.github.com>
2020-06-26 11:05:56 +0100

    Create MultipleChoice.ps1

---
+++ MultipleChoice.ps1
@@ -0,0 +1,22 @@
+$fruit = null
+function Main-Menu {
+do {
+    cls
+    Write-Host "Select your smoothie choice:"
+    Write-Host "1. Apple Smoothie `n2. Orange Fresh `n3. Banana Smoothie"
+    $menuresponse = read-host [Enter Selection]
+    Switch ($menuresponse) {
+        "1" {Write-Host "`nYou selected 1"
+                $global:fruit = "apple"}
+        "2" {Write-Host "`nYou selected 2"
+                $global:fruit = "orange"}
+        "3" {Write-Host "`nYou selected 3"
+                $global:fruit = "banana"}
+                            }
+    }
+    until (1..3 -contains $menuresponse) 
+}
+
+Main-Menu
+
+Write-Host $fruit
